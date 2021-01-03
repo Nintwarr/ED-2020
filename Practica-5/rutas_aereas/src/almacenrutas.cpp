519801e962bdf702553daa4b5bdcab691e9de8ea
Darío Megías Guerrero <d.megias.guerrero@gmail.com>
2021-01-03 19:13:33 +0100

    Pequeño error sintáctico arreglado

--- Practica-5/rutas_aereas/src/almacenrutas.cpp
+++ Practica-5/rutas_aereas/src/almacenrutas.cpp
@@ -12,34 +12,34 @@ void Almacen_Rutas::Insertar (const Ruta & R){
 void Almacen_Rutas::Borrar (const Ruta &R){
     map <string,Ruta>:: iterator it = ruta.find (R.GetCode());
     ruta.erase(it);
-    
+
 }
- 
+
 Ruta Almacen_Rutas::GetRuta (const string & a){
     map <string,Ruta>:: iterator it = ruta.find(a);
     return *it;
 }
 
-Almacen_Ruta::iterator Almacen_Ruta::begin(){
-    Almacen_Ruta::iterator it;
+Almacen_Rutas::iterator Almacen_Rutas::begin(){
+    Almacen_Rutas::iterator it;
     it.p = ruta.begin();
     return it;
 }
 
-Almacen_Ruta::const_iterator Almacen_Ruta::begin(){
-    Almacen_Ruta::const_iterator it;
+Almacen_Rutas::const_iterator Almacen_Rutas::begin(){
+    Almacen_Rutas::const_iterator it;
     it.p = ruta.cbegin();
     return it;
  }
 
-Almacen_Ruta::iterator Almacen_Ruta::end(){
-    Almacen_Ruta::iterator it;
+Almacen_Rutas::iterator Almacen_Rutas::end(){
+    Almacen_Rutas::iterator it;
     it.p = ruta.end();
     return it;
 }
 
-Almacen_Ruta::const_iterator Almacen_Ruta::end(){
-    Almacen_Ruta::const_iterator it;
+Almacen_Rutas::const_iterator Almacen_Rutas::end(){
+    Almacen_Rutas::const_iterator it;
     it.p = ruta.cend();
     return it;
  }
